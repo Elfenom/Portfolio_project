65c24517bdd4f88b6e2626cec3a3e0478bed4307
MadukaJustice <jonasjustice98@gmail.com>
2024-05-06 22:19:19 +0100

    Adding the home, features and project components

---
+++ src/components/features/Features.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { AiFillAppstore } from "react-icons/ai";
+import { FaMobile, FaGlobe } from "react-icons/fa";
+import { SiProgress, SiKalilinux } from "react-icons/si";
+import Title from "../layouts/Title";
+import Card from "./Card";
+
+const Features = () => {
+  return (
+    <section
+      id="features"
+      className="w-full py-20 border-b-[1px] border-b-black"
+    >
+      <Title Title="Features" des="What I Do" />
+      <div className='grid grid-cols-3 gap-20'>
+        <Card
+          title="Business Strategy"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
+          hic consequuntur eum repellendous ad."
+          
+        />
+        <Card
+          title="App Development"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
+          hic consequuntur eum repellendous ad."
+          icon={<AiFillAppstore />}
+        />
+        <Card
+          title="Cyber Security"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
+          hic consequuntur eum repellendous ad."
+          icon={<SiKalilinux  />}
+        />
+        <Card
+          title="SEO Optimization"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
+          hic consequuntur eum repellendous ad."
+          icon={<SiProgress />}
+        />
+        <Card
+          title="Mobile Development"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
+          hic consequuntur eum repellendous ad."
+          icon={<FaMobile />}
+        />
+        <Card
+          title="Hosting Websites"
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
+          hic consequuntur eum repellendous ad."
+          icon={<FaGlobe />}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Features;
