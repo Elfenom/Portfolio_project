a1db261f0f79e0b34b5b9514c022e17eb1edc3ab
MadukaJustice <jonasjustice98@gmail.com>
2024-06-21 19:14:56 +0100

    Update: implementing some new features

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -13,34 +13,30 @@ const Features = () => {
     >
       <Title Title="Features" des="What I Do" />
       <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-20'>
-        <Card
+        {/* <Card
           title="Business Strategy"
           des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
           hic consequuntur eum repellendous ad."
           
-        />
+        /> */}
         <Card
           title="Web Development"
-          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
-          hic consequuntur eum repellendous ad."
+          des="I develop reponsive websites with nice ui tailored to solve me clients needs"
           icon={<AiFillAppstore />}
         />
         <Card
           title="Cyber Security"
-          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
-          hic consequuntur eum repellendous ad."
+          des="With the ever-evolving threats in the cyberSpace, I secure networked systems and everything associated with the electronic side of life."
           icon={<SiKalilinux  />}
         />
         <Card
           title="Network Technician"
-          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
-          hic consequuntur eum repellendous ad."
+          des="I collaborate with my fellow network technician to reduce system downtime that may impede business operation."
           icon={<SiProgress />}
         />
         <Card
           title="Mobile Development"
-          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
-          hic consequuntur eum repellendous ad."
+          des="I develop mobile applications to cater different business needs"
           icon={<FaMobile />}
         />
         <Card
